e033f55c4bf1ba3cbdbcbfeb9751d483b9d56b14
Cumin Lo <CuCuCumin@gmail.com>
2019-09-25 16:24:22 +0800

    Delete Old & Dockerfile Add ext protobuf

--- php/Old/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#  author Cumin.Lo
-
-restart() {
-    /usr/local/bin/php ${SITE_WORK_DIR}/mp/artisan queue:restart
-    nohup /usr/local/bin/php ${SITE_WORK_DIR}/mp/artisan queue:work beanstalkd --queue=mp_high_me,mp_middle_me,mp_low_me,mp_default_me --sleep=1 --tries=3 --daemon &
-}
-
-restart
